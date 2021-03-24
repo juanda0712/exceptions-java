aea223d0760928e1f3b2341ca397c1d83851d3bd
Juan Rodriguez <juanrm071299@gmail.com>
2021-03-24 12:40:25 -0600

    Update checked exception
    add Scanner to enter the numbers

--- src/checkedException/division/Operations.java
+++ src/checkedException/division/Operations.java
@@ -1,10 +1,11 @@
-package checkedException.operations;
+package checkedException.division;
 
 public class Operations {
 
-    /**
+    /*
      * this function has an exception handling Checked
-     * the function that calls this function should handle the exception*/
+     * the function that calls this function should handle the exception
+     * the function is declared with with throws */
     public static Double division(Double a, Double b)throws Exception{
         if (b == 0){
             throw new Exception("El divisor no puede ser 0");
